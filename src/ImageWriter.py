185b22cba07f2fe136b8ccb287212d860bfaab87
Emin Fedar <eminfedar@gmail.com>
2020-04-21 21:20:51 +0300

    init: ImageWriter & USBDeviceManager classes.

---
+++ src/ImageWriter.py
@@ -0,0 +1,10 @@
+
+class ImageWriter:
+    def __init__(self):
+        pass
+
+    def setFilepath(self, filepath):
+        self.filepath = filepath
+
+    def setDevice(self, device):
+        self.device = device
---
+++ src/USBDeviceManager.py
@@ -0,0 +1,9 @@
+
+class USBDeviceManager:
+    def __init__(self):
+        pass
+
+    def getUSBDevices(self):
+        #deviceList = []
+        deviceList = [["/dev/usb1","Pardus","4GB"],["/dev/usb2","Pardu12s","32GB"],["/dev/usb3","Pardus2","16GB"],["/dev/usb4","Pardus44","8GB"]]
+        return deviceList
