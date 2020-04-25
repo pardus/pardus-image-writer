e04a81c7f219e6b0fc1b98a1ea38293e6f57026d
Emin Fedar <eminfedar@gmail.com>
2020-04-25 16:15:31 +0300

    added: ImageWriter

--- src/ImageWriter.py
+++ src/ImageWriter.py
@@ -1,10 +1,40 @@
+#!/usr/bin/env python3
 
-class ImageWriter:
-    def __init__(self):
-        pass
+import subprocess, sys, os, time
 
-    def setFilepath(self, filepath):
-        self.filepath = filepath
+drive = sys.argv[1]
+filepath = sys.argv[2]
 
-    def setDevice(self, device):
-        self.device = device
+# Unmount the drive before writing on it
+# subprocess.call(['umount', drive+'1'])
+
+bufferSize = 512
+writtenBytes = 0
+totalFileBytes = os.path.getsize(filepath)
+
+readFile = open(filepath, "rb", buffering=0)
+writeFile = open(drive, "wb", buffering=0)
+try:
+    oldMB = 0
+    readBytes = readFile.read(bufferSize)
+    while readBytes:
+        writeFile.write(readBytes)
+        readBytes = readFile.read(bufferSize)
+        writtenBytes += bufferSize
+
+        newMB = int(writtenBytes/1000/1000)
+        if oldMB != newMB:
+            oldMB = newMB
+            print(f"{writtenBytes} {totalFileBytes}")
+            sys.stdout.flush()
+    writeFile.flush()
+except IOError:
+    exit(1)
+else:
+    readFile.close()
+    writeFile.close()
+
+time.sleep(1)
+subprocess.call(['eject', drive])
+
+exit(0)
