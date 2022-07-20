7ab9d8adae39f87d5c51879df2626122232068c6
Fatih Altun <fatih.altun@pardus.org.tr>
2022-07-20 15:30:59 +0300

    Fix typo

--- setup.py
+++ setup.py
@@ -33,7 +33,6 @@ def create_mo_files():
 
 data_files = [
     ("/usr/share/applications/", ["tr.org.pardus.image-writer.desktop"]),
-    ("/usr/share/locale/tr/LC_MESSAGES/", ["translations/tr/LC_MESSAGES/pardus-image-writer.mo"]),
     ("/usr/share/pardus/pardus-image-writer/", ["icon.svg", "main.svg", "iso.svg", "disk.svg", "settings.svg", "uefi-ntfs.img"]),
     ("/usr/share/pardus/pardus-image-writer/src",
      ["src/main.py", "src/MainWindow.py", "src/ISOCopier.py", "src/ImageWriter.py", "src/USBDeviceManager.py",
